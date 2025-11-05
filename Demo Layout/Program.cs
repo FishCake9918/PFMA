29c4bed3efcd50aa452dff36822015fe2eac9e32
Vyle31231022150 <Vyle.31231022150@st.ueh.edu.vn>
2025-11-05 11:15:59 +0700

    hihi

--- Demo Layout/Program.cs
+++ Demo Layout/Program.cs
@@ -8,6 +8,8 @@ namespace Demo_Layout
         [STAThread]
         static void Main()
         {
+
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
